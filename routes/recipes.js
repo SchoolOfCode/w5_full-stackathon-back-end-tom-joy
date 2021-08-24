7a5e6029e9eea5a7611fc092e889093813b0c500
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-08-24 11:07:21 +0100

    routes added

---
+++ routes/recipes.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+const {
+  getAllRecipes,
+  getRecipesByCountry,
+  deleteRecipeById,
+  postNewRecipe,
+  updateRecipeById,
+  patchRecipeById,
+} = require("../models/recipes");
+
+// get all data from recipes
+router.get("/", async (req, res) => {
+  const recipes = await getAllRecipes();
+  res.json({
+    message: "Here are the recipes",
+    payload: recipes,
+  });
+});
+
+// get data by country name
+router.get("/", async (req, res) => {
+  const { search } = req.query;
+  if (search) {
+    const recipesByCountry = await getRecipesByCountry("country", search);
+    res.json({
+      message: `Here are the recipes of ${country}`,
+      payload: recipesByCountry,
+    });
+  }
+});
