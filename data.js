bbbc8368d4fcfc65abec53aea070c243f41382c7
tomlowen <tom.r.lowen@googlemail.com>
2021-08-24 10:35:32 +0100

    Created some initial data

---
+++ data.js
@@ -0,0 +1,162 @@
+const recipes = [
+    {
+        country: "South Africa",
+        recipe: "Bobotie",
+        ingredients: [
+            "3 slices of bread",
+            "1 cup + ½ cup milk",
+            "2 tablespoons olive oil",
+            "2 large onions",
+            "4 teaspoons medium curry powder",
+            "1 teaspoon dried herbs",
+            "1 teaspoon ground cumin",
+            "½ teaspoon ground turmeric",
+            "2 garlic cloves, finely chopped",
+            "1½ pound ground beef",
+            "½ cup fruit chutney",
+            "1 tablespoon apricot jam",
+            "Zest and juice of one medium lemon",
+            "4 teaspoons tomato paste",
+            "Salt and pepper",
+            "2 large eggs",
+            "4 bay leaves",
+        ],
+        steps: "Preheat your oven to 350°F. Soak the bread in 1 cup of the milk. Heat the olive oil in a wide skillet set over medium heat. Once the oil is hot, add the onions, and cook until soft. \
+        \
+        Add the curry powder, mixed herbs, cumin, turmeric and garlic, and stirring constantly, allow to cook for a minute or two until the garlic is soft. \
+        \
+        Add the ground beef, and cook, stirring frequently to break up any big chunks, until browned. Once the beef is browned, remove the skillet from the heat, and stir in the chutney, apricot jam, all the lemon zest, half the lemon juice, tomato paste, and salt and pepper to taste. \
+        \
+        Mix well, give it a taste, and add more lemon juice, salt, and pepper as desired. It should be delightfully zingy! Squeeze the milk from the bread, reserving the milk for later, and smooshing and tearing the bread into small pieces. \
+        \
+        Mix the bread into the beef mixture, and spread evenly into an oven proof dish. Strain the milk that has come from the bread, and add the remaining ½ cup of milk. \
+        \
+        Beat in the eggs, and season with ¼ teaspoon salt and pepper to taste. Pour this over the meat, and decoratively scatter the bay leaves on top. \
+        \
+        Bake, uncovered, at 350°F for 45 minutes, or until golden brown." 
+    },
+    {
+        country: "Japan",
+        recipe: "Salmon Poke Sushi Bowl",
+        ingredients: [
+            "Rice",
+            "Sushi Vinegar",
+            "Salmon Cubes",
+            "Poke Sauce",
+            "Mixed Sesame",
+            "Wakame Salad",
+            "Edamame",
+            "Avocado",
+            "Carrot",
+        ],
+        steps: "Place the rice in a sieve over a saucepan and rinse with cold water to remove the starch. Replace with fresh water 3-4 times until the water turns clear. \
+        \
+        After cleaning the rice, put 350ml of cold water and the rice into a pan and leave to soak for 30 minutes. Place over a hob, and bring the water to a boil on a high heat with the lid on. \
+        \
+        As soon as it is bubbling, reduce to the lowest setting for 12 minutes, keeping the lid on.\
+        \
+        Don't lift the lid at al during the cooking process. After 12 minutes, take off the heat and rest for 20 minutes.\
+        \
+        Transfer to a large mixing bowl. Sprinkle 70ml of sushi vinegar onto the rice, mixing it well, so that all the rice grains are coated.\
+        \
+        After mixing in the vinegar, leave to cool for a further 30 minutes, after which the rice is ready to make poke and sushi.\
+        \
+        Separate the cooled rice into two bowls for the poke.\
+        \
+        To assemble, open the salmon - already sliced and cubed, and place half of the portion onto each side of the rice bowl, followed by the green wakame salad.\
+        \
+        Slice the avocado into half, remove the stone, cube the avocado and add half to each bowl.\
+        \
+        Add half the shredded carrot and edamame to the middle of each bowl.\
+        \
+        Drizzle each bowl with the poke sauce and finish with a sprinkle of sesame seeds." 
+    },
+    {
+        country: "Spain",
+        recipe: "Classic Spanish paella",
+        ingredients: [
+            "large pinch saffron strands",
+            "1 vegetable or chicken stock cube",
+            "3 tbsp olive oil plus extra for drizzling",
+            "125g chorizo roughly chopped",
+            "500g boneless skinless chicken breasts or thighs",
+            "1 onion finely chopped",
+            "3 garlic cloves finely chopped",
+            "1 red pepper",
+            "2 tsp paprika",
+            "250g Spanish paella rice",
+            "4 medium tomatoes",
+            "75g frozen peas",
+            "250g cooked prawns with shells on (thawed if frozen) and rinsed",
+            "small handful flat-leaf parsley",
+            "chopped lemon wedges",
+        ],
+        steps: "Stir the saffron strands into the stock and set aside to infuse while you prepare the rest of the paella. \
+        \
+        Heat 1 tbsp oil in a paella pan or a large deep frying pan with a lid. Tip in the chorizo and fry for about 3 mins until crisp and the oil has been released. Remove the chorizo and drain on kitchen paper, leaving the oil in the pan.\
+        \
+        Stir the chicken into the pan and fry over a high heat for 7-8 mins, or until the meat is golden and cooked through. Transfer the chicken to a bowl and set aside.\
+        \
+        Pour another 1 tbsp of oil into the pan, tip in the chopped onion and garlic and stir-fry for 4-5 mins, until softened and just starting to colour. Stir in the pepper and paprika with the remaining tablespoon of oil and stir-fry for a further 1-2 mins. The pan should have lots of crispy, brown bits on the bottom, which will all add flavour.\
+        \
+        With the heat still quite high, quickly stir in the rice so it is well-coated in the oil, then pour in the saffron-infused stock plus 450ml boiling water, scraping up the sticky brown bits from the bottom of the pan with a wooden spoon.\
+        \
+        Return the browned chicken pieces to the pan, then add the chopped tomatoes. Cover the pan and cook on a medium heat for 10 mins, stirring once or twice. Scatter the peas, prawns and fried chorizo over the top, cover again and leave to cook a further 5-10 mins, or until the rice is just cooked and most of the liquid in the pan has been absorbed.\
+        \
+        Remove the pan from the heat, put the lid on and leave to rest for 5 mins. Stir a few times to mix the ingredients, season to taste and scatter over the chopped parsley. Serve with lemon wedges and an extra drizzle of oil, if you like." 
+    },
+    {
+        country: "Mexico",
+        recipe: "Queso fundido",
+        ingredients: [
+            "2 x 130g packs diced chorizo",
+            "1 small onion diced",
+            "175g mature cheddar grated",
+            "50g red Leicester grated",
+            "125g soft cheese at room temperature",
+            "150g tomatoes diced",
+            "2 tbsp jalapeños diced",
+            "2 tbsp parsley chopped",
+            "2 tsp white wine vinegar",
+            "1 tsp dried oregano",
+            "200g tortilla chips",
+        ],
+        steps: "Preheat the oven to gas 6, 200°C, fan 180°C. Put the diced chorizo in a cold, dry frying pan with the onion. Cook over a medium heat until the onion has softened and the chorizo is golden and crisp. Transfer to a sieve to drain away excess oil.\
+        \
+        Mix the grated Cheddar and red Leicester with the soft cheese and some black pepper. In an ovenproof frying pan or baking dish, layer up spoonfuls of the chorizo and cheese mixtures. Bake in the oven for 15-20 mins, until bubbling.\
+        \
+        Meanwhile, make a salsa by mixing together the diced tomatoes, jalapeños, chopped parsley or coriander, white wine vinegar, dried oregano and some seasoning. \
+        \
+        Spoon some of the salsa over the top of the dish. Serve the rest alongside, with tortilla chips for scooping while the cheese is still gooey." 
+    },
+    {
+        country: "Australia",
+        recipe: "Lamingtons",
+        ingredients: [
+            "125 g / 4.5 oz unsalted butter",
+            "1 cup (220g) caster sugar",
+            "1/2 tsp vanilla extract",
+            "3 eggs",
+            "1 3/4 cups (260g) flour",
+            "3.5 tsp baking powder",
+            "1/2 cup (125 ml) milk (low or full fat)",
+            "4 cups (480g) icing sugar (confectionary sugar)",
+            "1/3 cup (22g) cocoa powder",
+            "1 tbsp (15g) unsalted butter",
+            "150ml (1/2 cup + 2 tbsp) boiling water",
+            "3 - 4 cups desiccated coconut",
+            "Strawberry jam",
+            "1 cup (250 ml) cream",
+            "1 tbsp white sugar",
+        ],
+        steps: "Preheat the oven to gas 6, 200°C, fan 180°C. Put the diced chorizo in a cold, dry frying pan with the onion. Cook over a medium heat until the onion has softened and the chorizo is golden and crisp. Transfer to a sieve to drain away excess oil.\
+        \
+        Mix the grated Cheddar and red Leicester with the soft cheese and some black pepper. In an ovenproof frying pan or baking dish, layer up spoonfuls of the chorizo and cheese mixtures. Bake in the oven for 15-20 mins, until bubbling.\
+        \
+        Meanwhile, make a salsa by mixing together the diced tomatoes, jalapeños, chopped parsley or coriander, white wine vinegar, dried oregano and some seasoning. \
+        \
+        Spoon some of the salsa over the top of the dish. Serve the rest alongside, with tortilla chips for scooping while the cheese is still gooey." 
+    },
+]
+
+module.exports = recipes;
