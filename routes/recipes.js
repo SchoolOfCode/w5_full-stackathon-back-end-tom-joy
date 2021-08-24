14ce2c82680e072ab990736b2d9a1d863e28bf4c
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-08-24 11:56:46 +0100

    post method added

--- routes/recipes.js
+++ routes/recipes.js
@@ -9,8 +9,17 @@ const {
   patchRecipeById,
 } = require("../models/recipes");
 
-// get all data from recipes
+// get all data from recipes & get data by country name
 router.get("/", async (req, res) => {
+  const { country } = req.query;
+  if (country) {
+    const recipesByCountry = await getRecipesByCountry(country);
+    res.json({
+      message: `Here are the recipes of ${country}`,
+      payload: recipesByCountry.rows,
+    });
+    return;
+  }
   const recipes = await getAllRecipes();
   res.json({
     message: "Here are the recipes",
@@ -18,16 +27,25 @@ router.get("/", async (req, res) => {
   });
 });
 
-// get data by country name
-router.get("/", async (req, res) => {
-  const { search } = req.query;
-  if (search) {
-    const recipesByCountry = await getRecipesByCountry(search);
-    res.json({
-      message: `Here are the recipes of ${search}`,
-      payload: recipesByCountry.rows,
-    });
-  }
+// delete recipe by id
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await deleteRecipeById(id);
+  res.json({
+    success: true,
+    message: `recipe ${id} deleted!`,
+  });
+});
+
+// Post new recipe
+router.post("/", async (req, res) => {
+  const { body } = req;
+  const data = await postNewRecipe(body);
+  res.json({
+    success: true,
+    message: `new recipe added!`,
+    payload: "test",
+  });
 });
 
 module.exports = router;
